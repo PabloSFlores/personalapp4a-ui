0fcba821221ecc1b222816d41cfcc769717b8664
PabloSFlores <105692302+PabloSFlores@users.noreply.github.com>
2022-11-10 09:58:52 -0600

    Segundo commit
    Proyecto de Angular CLI 14

--- personalapp4a/src/app/app.component.ts
+++ personalapp4a/src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'personalapp4a';
+  counter: number = 0;
+
+  adding(){
+    this.counter++;
+  }
+
+  minus(){
+    this.counter--;
+  }
 }
---
+++ personalapp4a/src/app/heroes/heroes.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { HeroesMainPageComponent } from './main-page/main-page.component';
+import { ListComponent } from './list/list.component';
+
+@NgModule({
+  declarations: [HeroesMainPageComponent, ListComponent], //Todos los componentes del modulo
+  imports: [], //Solo se agregan otros modulos
+  providers: [], //Se agregan los servicios
+  exports: [HeroesMainPageComponent],
+})
+
+export class HeroesModule{
+
+}
