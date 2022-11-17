1586939ac4f1f0822a54cec54adf1b3891aa4a4f
PabloSFlores <105692302+PabloSFlores@users.noreply.github.com>
2022-11-17 09:22:32 -0600

    Mejor versión de Node JS
    Token, servicios

---
+++ personal4aa/src/modules/controller/routes.js
@@ -0,0 +1,8 @@
+const { personalRouter } = require('./personal/personal.controller');
+const { userRouter } = require('./user/user.controller')
+const { authRouter } = require('./auth/auth.controller')
+module.exports = {
+  personalRouter,
+  userRouter,
+  authRouter,
+};
